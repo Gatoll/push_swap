59c8c129831d71f50e249088b34f9888847ecd81
Kanon Okazaki <kaokazak@c3r1s3.42tokyo.jp>
2024-09-16 13:40:03 +0900

    done

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: kaokazak <kaokazak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 16:06:32 by kaokazak          #+#    #+#             */
-/*   Updated: 2024/09/13 21:37:32 by kaokazak         ###   ########.fr       */
+/*   Updated: 2024/09/16 13:39:10 by kaokazak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	is_sorted(t_stack **stack)
 {
 	t_stack	*current;
 
+	if (!stack || !(*stack))
+		return (FALSE);
 	current = *stack;
 	while (current->next)
 	{
@@ -54,6 +56,8 @@ int	serch_min(t_stack **stack, int *len_to_min)
 	int		count;
 	t_stack	*current;
 
+	if (!stack || !(*stack))
+		return (FALSE);
 	min = (*stack)->value;
 	current = (*stack)->next;
 	count = 0;
